937782b1c74466858163346818fe17ba0ad6e1b3
Atharva <chilwerwaratharva@gmail.com>
2025-01-10 10:14:58 +0530

    Included find max value in Array List method

--- ArrayLists/ArrayList1.java
+++ ArrayLists/ArrayList1.java
@@ -14,13 +14,23 @@ public class ArrayList1 {
         System.out.println(list.size());
 
         reverseAL(list);
+        findMaxInAL(list);
     }
 
     public static void reverseAL(ArrayList lt){
         for(int i= lt.size()-1;i>=0;i--){
             System.out.print(lt.get(i) + " ");
         }
+        System.out.println();
     }
 
-    
+    public static void findMaxInAL(ArrayList<Integer> lt){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<lt.size();i++){
+            // if(lt.get(i)>max) max = lt.get(i);
+            max = Math.max(max, lt.get(i));
+        }
+        
+        System.out.println("Max element:" + max);
+    }
 }
